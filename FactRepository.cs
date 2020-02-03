69db71d0c7209b066cc46d8ec3510318b4e760b9
Jazo74 <55430157+Jazo74@users.noreply.github.com>
2020-02-03 12:49:55 +0100

    more advance

--- FactRepository.cs
+++ FactRepository.cs
@@ -5,12 +5,13 @@ namespace ExpertSystem
 {
     public class FactRepository
     {
+        List<Fact> factList = new List<Fact>();
         public FactRepository()
         {
         }
         public void AddFact(Fact fact)
         {
-
+            string ID = AnyInput()
         }
         public IEnumerator<Fact> GetIterator()
         {

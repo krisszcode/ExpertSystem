b4b18ecaf9f2ae3e69b48b4299fb5d328c7b8111
krisszcode <kicsikrissz111@gmail.com>
2020-02-03 16:05:03 +0100

    ans and values

---
+++ MultipleValue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpertSystem
+{
+    class MultipleValue : Value
+    {
+        public List<string> Params = new List<string>();
+        public bool selectionType;
+        public MultipleValue(List<string> Params, bool selectionType)
+        {
+            this.Params = Params;
+            this.selectionType = selectionType;
+        }
+    }
+}
---
+++ SingleValue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpertSystem
+{
+    class SingleValue : Value
+    {
+        string Param;
+        bool selectionType;
+        SingleValue(String Param, bool selectionType)
+        {
+            this.Param = Param;
+            this.selectionType = selectionType;
+        }
+    }
+}

3f3ee1e87857ea6fc6ca08d406e4e6d56ccf066e
viktorferenczi <ferencziviktor11@gmail.com>
2020-02-04 11:02:50 +0100

    question class implement

--- Question.cs
+++ Question.cs
@@ -3,32 +3,36 @@ namespace ExpertSystem
 {
     public class Question
     {
-
+        string ID { get; set; }
+        string question { get; set; }
+        Answer Answer;
 
         public Question(string id, string question, Answer answer)
         {
-
+            ID = id;
+            this.question = question;
+            Answer = answer;
         }
 
         public string GetID()
         {
-            return null;
+            return ID;
         }
 
 
         public string GetQuestion()
         {
-            return null;
+            return question;
         }
 
         public Answer GetAnswer()
         {
-            return null;
+            return Answer;
         }
 
         public bool GetEvalutedAnswer(string input)
         {
-            return false;
+            return Answer.evaluateAnswerByInput(input);
         }
 
        
